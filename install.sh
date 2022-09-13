0e449d92588cb7618f797943c804f020c0f37a34
Nguyễn Văn Khánh <88880309+KhanhNguyen9872@users.noreply.github.com>
2022-09-13 19:12:19 +0700

    remove terminal history

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
 # Variable
+rm -rf $HOME/.bash_history > /dev/null 2>&1
 sudo="$(which sudo)"
 apache2_conf="/etc/apache2/apache2.conf"
 apache2_security="/etc/apache2/conf-available/security.conf"
